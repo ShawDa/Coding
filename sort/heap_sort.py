59fbf57076628a38022ca8af6900a30bb24de626
ShawDa <1315193735@qq.com>
2018-09-26 17:58:56 +0800

    Add files via upload

---
+++ sort/heap_sort.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+import random
+
+
+def random_int_list(start, stop, length):
+    start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
+    length = int(abs(length)) if length else 0
+    random_list = []
+    for i in range(length):
+        random_list.append(random.randint(start, stop))
+    return random_list
+
+
+def heap_sort(arr):
+    def max_heapify(start, end):
+        # 调整最大堆
+        root = start
+        while True:
+            child = 2 * root + 1
+            if child > end:  # 无子节点
+                break
+            if child + 1 <= end and arr[child] < arr[child+1]:  # 找较大子节点
+                child += 1
+            if arr[root] < arr[child]:
+                arr[root], arr[child] = arr[child], arr[root]  # 交换父子节点
+                root = child
+            else:
+                break
+
+    # 创建最大堆
+    for start in range((len(arr)//2) - 1, -1, -1):
+        max_heapify(start, len(arr)-1)
+
+    for end in range(len(arr) - 1, 0, -1):
+        arr[0], arr[end] = arr[end], arr[0]  # 将最大值置后
+        max_heapify(0, end - 1)
+    return arr
+
+
+if __name__ == '__main__':
+    data = random_int_list(1, 100, 10)
+    print(data)
+    print(heap_sort(data))
--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -12,13 +12,59 @@ def random_int_list(start, stop, length):
     return random_list
 
 
-class QuickSort:
-    def __init__(self,data):
-        self.data = data
+def quick_sort0(arr):
+    length = len(arr)
+    if length <= 1:
+        return arr
+    first = arr.pop(0)
+    less, more = [], []
+    for num in arr:
+        if num < first:
+            less.append(num)
+        else:
+            more.append(num)
+    return quick_sort0(less) + [first] + quick_sort0(more)
 
+
+def quick_sort1(arr, left, right):
+    def partition(ar, left, right):
+        cand = ar[right]
+        i = left
+        for j in range(left, right):
+            if ar[j] < cand:
+                ar[i], ar[j] = ar[j], ar[i]
+                i += 1
+        ar[i], ar[right] = ar[right], ar[i]
+        return i
+    if left < right:
+        q = partition(arr, left, right)
+        quick_sort1(arr, left, q-1)
+        quick_sort1(arr, q+1, right)
+    return arr
+
+
+def quick_sort2(arr, left, right):
+    if left >= right:
+        return
+    stack = [left, right]
+    while stack:
+        low, high = stack.pop(0), stack.pop(0)
+        if high - low <= 0:
+            continue
+        cand = arr[high]
+        i = low
+        for j in range(low, high):
+            if arr[j] < cand:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+        arr[high], arr[i] = arr[i], arr[high]
+        stack.extend([low, i-1, i+1, high])
+    return arr
 
 
 if __name__ == '__main__':
     data = random_int_list(1, 100, 10)
     print(data)
-
+    # print(quick_sort0(data))
+    # print(quick_sort1(data, 0, len(data)-1))
+    print(quick_sort2(data, 0, len(data)-1))
